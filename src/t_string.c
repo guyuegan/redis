0b76fc8d9d07671abfba2e5b6342b128bdefe212
guyuegan <15308999782hm>
2022-08-23 20:11:37 +0800

    push from vscode

--- src/t_string.c
+++ src/t_string.c
@@ -34,6 +34,7 @@
  * String Commands
  *----------------------------------------------------------------------------*/
 
+//检查字符是否超过最长限制：512MB
 static int checkStringLength(client *c, long long size) {
     if (size > 512*1024*1024) {
         addReplyError(c,"string exceeds maximum allowed size (512MB)");
